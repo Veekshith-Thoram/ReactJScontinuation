acb346a6fb578b72de9c419501ebbaac41e848c5
Veekshith-Thoram <veekshiththoram@gmail.com>
2021-06-02 23:16:37 +0530

    initial commit

---
+++ new-app/src/components/LifeCycleA.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class LifeCycleA extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default LifeCycleA
