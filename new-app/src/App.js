f617de0a4ebe66422a93ad083e598a2ce01ed546
Veekshith-Thoram <veekshiththoram@gmail.com>
2021-06-03 23:10:23 +0530

    refs and class refs

--- new-app/src/App.js
+++ new-app/src/App.js
@@ -4,11 +4,15 @@ import LifeCycleA from './components/LifeCycleA';
 import FragmentDemo from './components/FragmentDemo';
 import Table from './components/Table';
 import ParentComp from './components/ParentComp';
+import RefsDemo from './components/RefsDemo';
+import ParentRef from './components/ParentRef';
 
 function App() {
   return (
     <div className="App">
-      <ParentComp />
+      <ParentRef />
+      {/* <RefsDemo /> */}
+      {/* <ParentComp /> */}
       {/* <Table /> */}
       {/* <FragmentDemo /> */}
     {/* <LifeCycleA />       */}
